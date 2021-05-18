20dfdaa91bab7d279706a88d1a6ae6bf93636fe8
Sue Kim Paulsen <suekimpaulsen@gmail.com>
2021-05-18 18:52:58 -0500

    start writing localstorage code with notes

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,3 +1,20 @@
 $(document).ready(function() {
     console.log('ready');
 });
+
+
+
+//localstorage
+
+$('#hour9 .description')
+$('#hour10 .description')
+$('#hour11 .description')
+$('#hour12 .description')
+$('#hour13 .description')
+$('#hour14 .description')
+$('#hour15 .description')
+$('#hour16 .description')
+$('#hour17 .description')
+
+
+//moment
