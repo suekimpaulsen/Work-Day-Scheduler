2ed0a51e191088fda42734887e620a46b093b093
Sue Kim Paulsen <suekimpaulsen@gmail.com>
2021-05-18 22:13:56 -0500

    displaying m/d/y

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,20 +1,26 @@
-$(document).ready(function() {
-    console.log('ready');
-});
+// $(document).ready(function() {
+//     console.log('ready');
+// });
 
 
 
 //localstorage
 
-$('#hour9 .description')
-$('#hour10 .description')
-$('#hour11 .description')
-$('#hour12 .description')
-$('#hour13 .description')
-$('#hour14 .description')
-$('#hour15 .description')
-$('#hour16 .description')
-$('#hour17 .description')
+// $('#hour9 .description')
+// $('#hour10 .description')
+// $('#hour11 .description')
+// $('#hour12 .description')
+// $('#hour13 .description')
+// $('#hour14 .description')
+// $('#hour15 .description')
+// $('#hour16 .description')
+// $('#hour17 .description')
 
 
 //moment
+$("#currentDay").html(moment().format("dddd, MMMM Do YYYY"))
+
+//save button
+$(".saveBtn").click (function() {
+    console.log('save')
+})
