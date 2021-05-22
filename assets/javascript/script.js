db175d135ea87487df96697eda80d48897c922bd
Sue Kim Paulsen <suekimpaulsen@gmail.com>
2021-05-21 22:59:28 -0500

    clearBtn added. localstorage working

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,61 +1,51 @@
-// $(document).ready(function() {
-//     console.log('ready');
-// });
+$(document).ready(function() {
+    //display current day
+    $("#currentDay").html(moment().format("dddd, MMMM Do YYYY"))
 
+    //moment time block
+    function timeTrack() {
+        $(".time-block").each(function() {
+            var currentTime = moment().hour();
+            var blockTime = parseInt($(this).attr("id").split("hour")[1]);
 
-//moment
-$("#currentDay").html(moment().format("dddd, MMMM Do YYYY"))
+            if (blockTime < currentTime) {
+                $(this).addClass("past");
+                $(this).removeClass("present");
+                $(this).removeClass("future");
+            }
+            else if (blockTime === currentTime) {
+                $(this).removeClass("past");
+                $(this).addClass("present");
+                $(this).removeClass("future");
+            }
+            else {
+                $(this).removeClass("past");
+                $(this).removeClass("present");
+                $(this).addClass("future");
+            }
+        })
+    }
+    timeTrack();
 
-//moment time block
-function timeTrack() {
-    $(".time-block").each(function() {
-        var currentTime = moment().hour();
-        var blockTime = parseInt($(this).attr("id").split("hour")[1]);
-
-        console.log(currentTime);
-        console.log(blockTime);
-    
-        if (blockTime < currentTime) {
-            $(this).addClass("past");
-            $(this).removeClass("present");
-            $(this).removeClass("future");
-        }
-        else if (blockTime === currentTime) {
-            $(this).removeClass("past");
-            $(this).addClass("present");
-            $(this).removeClass("future");
-        }
-        else {
-            $(this).removeClass("past");
-            $(this).removeClass("present");
-            $(this).addClass("future");
-        }
+    //save button
+    $(".saveBtn").click (function() {
+        var text = $(this).siblings(".description").val();
+        var time = $(this).parent().attr("id");
+        localStorage.setItem(time, text);
+        console.log(localStorage)
     })
-}
-timeTrack();
+    $("#hour9 .description").val(localStorage.getItem("hour9"))
+    $("#hour10 .description").val(localStorage.getItem("hour10"))
+    $("#hour11 .description").val(localStorage.getItem("hour11"))
+    $("#hour12 .description").val(localStorage.getItem("hour12"))
+    $("#hour13 .description").val(localStorage.getItem("hour13"))
+    $("#hour14 .description").val(localStorage.getItem("hour14"))
+    $("#hour15 .description").val(localStorage.getItem("hour15"))
+    $("#hour16 .description").val(localStorage.getItem("hour16"))
+    $("#hour17 .description").val(localStorage.getItem("hour17"))
 
-
-
-//localstorage
-
-$('#hour9 .description').val(window.localStorage.getItem('hour9'));
-$('#hour10 .description').val(localStorage.getItem('hour10'));
-$('#hour11 .description').val(localStorage.getItem('hour11'));
-$('#hour12 .description').val(localStorage.getItem('hour12'));
-$('#hour13 .description')
-$('#hour14 .description')
-$('#hour15 .description')
-$('#hour16 .description')
-$('#hour17 .description')
-
-
-//save button
-$(".saveBtn").click (function() {
-    var text = $(this).siblings(".description").val();
-    var time = $(this).parent().attr("id");
-    localStorage.setItem("time", time);
-    localStorage.setItem("text", text);
-    console.log(time)
-    console.log(localStorage)
-})
-
+    //clear button
+    $(".clearBtn").click (function() {
+        localStorage.clear();
+    })
+});
